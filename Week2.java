e2585f929d5e6a2b4bc5f5cd4c1fb92dd1c6ad98
Nemalikonda Saketh <92433733+sakethnemalikonda@users.noreply.github.com>
2025-03-26 00:22:32 +0530

    Update Week2.java

--- Week2.java
+++ Week2.java
@@ -5,37 +5,40 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 public class Week2 {
-	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
-		WebDriver driver=new ChromeDriver();
+    public static void main(String[] args) throws InterruptedException {
+        // Initialize WebDriver
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize(); // Maximize browser window
         driver.get("https://www.mercurytravels.co.in/");
-        Actions actions=new Actions(driver);
-        WebElement CustomerLogin=driver.findElement(By.xpath("/html/body/nav[2]/div/div[2]/ul/li[1]/a"));
+        Thread.sleep(2000);
+        // Initialize Actions class
+        Actions actions = new Actions(driver);
+        // Hover over "Customer Login"
+        WebElement CustomerLogin = driver.findElement(By.xpath("//a[contains(text(),'Customer Login')]"));
         actions.moveToElement(CustomerLogin).build().perform();
-        WebElement Register=driver.findElement(By.xpath("/html/body/nav[2]/div/div[2]/ul/li[1]/ul/li[2]/a"));
+        Thread.sleep(1000);
+        // Click on "Register"
+        WebElement Register = driver.findElement(By.xpath("//a[contains(text(),'Register')]"));
         Register.click();
         Thread.sleep(2000);
-        WebElement FirstName=driver.findElement(By.name("first_name"));
-        FirstName.sendKeys("Saketh");
-        Thread.sleep(2000);
-        WebElement LastName=driver.findElement(By.name("last_name"));
-        LastName.sendKeys("Nemalikonda");
-        Thread.sleep(2000);
-        WebElement EmailAddress=driver.findElement(By.id("acc_user_email"));
-        EmailAddress.sendKeys("sakethnemalikonda@gmail.com");
-        Thread.sleep(2000);
-        WebElement Set_a_Password=driver.findElement(By.id("acc_user_password"));
-        Set_a_Password.sendKeys("12345");
-        Thread.sleep(2000);
-        WebElement ConfirmPassword=driver.findElement(By.id("acc_user_passconf"));
-        ConfirmPassword.sendKeys("12345");
-        Thread.sleep(2000);
-        WebElement MobileNumber=driver.findElement(By.id("acc_mobile_no"));
-        MobileNumber.sendKeys("8121058116");
-        Thread.sleep(2000);
-        WebElement Reg_Button=driver.findElement(By.xpath("//*[@id=\"modalUserLogin\"]/div/div/div[2]/form/button"));
+        // Fill registration form
+        driver.findElement(By.name("first_name")).sendKeys("Saketh");
+        Thread.sleep(1000);
+        driver.findElement(By.name("last_name")).sendKeys("Nemalikonda");
+        Thread.sleep(1000);
+        driver.findElement(By.id("acc_user_email")).sendKeys("sakethnemalikonda@gmail.com");
+        Thread.sleep(1000);
+        driver.findElement(By.id("acc_user_password")).sendKeys("12345");
+        Thread.sleep(1000);
+        driver.findElement(By.id("acc_user_passconf")).sendKeys("12345");
+        Thread.sleep(1000);
+        driver.findElement(By.id("acc_mobile_no")).sendKeys("8121058116");
+        Thread.sleep(1000);
+        // Click Register Button
+        WebElement Reg_Button = driver.findElement(By.xpath("//button[contains(text(),'Register')]"));
         Reg_Button.click();
-        Thread.sleep(2000);
-        driver.close();
-	}
+        Thread.sleep(3000); // Allow time to process registration
+        System.out.println("Registration form submitted successfully!");
+        driver.close(); // Close browser
+    }
 }
