efb13cb4465b96854132dffa289d383605084e7a
Nemalikonda Saketh <92433733+sakethnemalikonda@users.noreply.github.com>
2025-03-25 23:33:38 +0530

    Update Week2.java

--- Week2.java
+++ Week2.java
@@ -1,13 +1,10 @@
 package cseb5x0;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-
 public class Week2 {
-
 	public static void main(String[] args) throws InterruptedException{
 		// TODO Auto-generated method stub
 		WebDriver driver=new ChromeDriver();
@@ -41,5 +38,4 @@ public class Week2 {
         Thread.sleep(2000);
         driver.close();
 	}
-
 }
