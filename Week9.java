58b004554bbb6f5771d68abaed5337e455b2d407
Nemalikonda Saketh <92433733+sakethnemalikonda@users.noreply.github.com>
2025-04-17 15:57:40 +0530

    Update Week9.java

--- Week9.java
+++ Week9.java
@@ -1,17 +1,25 @@
-package ruthvika;
+package week9;
 import java.awt.AWTException;
 import java.util.List;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-public class week8 {
+public class week9 {
 	public static void main(String[] args) throws InterruptedException,AWTException{
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\student\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com");
-		List<WebElement> m=driver.findElements(By.xpath(null));
+		Thread.sleep(2000);
+		WebElement searchBar = driver.findElement(By.name("q"));
+		searchBar.sendKeys("Amazon");
+		searchBar.sendKeys(Keys.ENTER);
+		Thread.sleep(15000);
+		WebElement amazonLink = driver.findElement(By.xpath("//*[@id=\"tads\"]/div[3]/div/div/div/div[1]/a/div[1]/span"));
+        amazonLink.click();
+        Thread.sleep(5000);
+		List<WebElement> m=driver.findElements(By.xpath(""));
 		for(int i=0;i<m.size();i++) {
 			String s=m.get(i).getText();
 			System.out.println("Text is: "+s);
